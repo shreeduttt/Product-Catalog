0de12e40d7ae4d73006955763472cf7a9882b5a3
shreedutt tiwari <shreedutttiwari@shreedutts-MacBook-Air.local>
2026-05-12 23:24:41 +0530

    feat: Implements simple loader

--- src/RatingHeatMap/ProductHeatmap.jsx
+++ src/RatingHeatMap/ProductHeatmap.jsx
@@ -2,6 +2,7 @@ import Header from '../Header/Header';
 import './Heatmap.css';
 import { useEffect, useState } from 'react';
 import RatingCard from './RatingCard';
+import Loader from '../Loader/Loader';
 
 const ProductHeatmap = () => {
     const [products, setProducts] = useState([]);
@@ -9,18 +10,19 @@ const ProductHeatmap = () => {
     const fetchData = async() => {
         const data = await fetch("https://dummyjson.com/products?limit=100");
         const json = await data.json();
-        setProducts(json.products);
+        setTimeout(() => setProducts(json.products), 500); // mimic heavy loading
+        
     }
 
     useEffect(() => {
        fetchData();
     }, [])
-
+    console.log(products.length);
     return (
         <div className='heatmap-container'>
             <Header title="Product Ratings and Heatmap🔥"/>
             <div className='heatmap-grid'>
-            {
+            { products.length > 0 ?
                 products.map((product) => (
                     <RatingCard 
                         key={product.id} 
@@ -30,6 +32,8 @@ const ProductHeatmap = () => {
                         description={product.description}
                     />
                 ))
+                :
+                <Loader/>
             }
             </div>
         </div>
